e10e65231f6eeb0fea4dc79b1536e21be7e009d5
devwjdalsdl07 <dev.wjdalsdl07@gmail.com>
2023-06-15 18:05:58 +0900

    feat: Firebase 회원가입 진행 완료

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import SignUpDiv from "../style/UserCss";
 import { useNavigate } from "react-router-dom";
+// firebase 연동
+import firebase from "../firebase";
 
 const SignUp = () => {
   const navigate = useNavigate();
@@ -8,9 +10,21 @@ const SignUp = () => {
   const [email, setEmail] = useState("");
   const [pw, setPw] = useState("");
   const [pwConfirm, setPwConfirm] = useState("");
-  const handleSignUp = e => {
+  const handleSignUp = async e => {
     e.preventDefault();
-    // Firebase 에 회원가입 하기
+    try {
+      // Firebase 에 회원가입 하기
+      let creatUser = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, pw);
+
+      await creatUser.user.updateProfile({
+        name: nickname,
+      });
+      console.log("등록된 정보", creatUser.user);
+    } catch (err) {
+      console.log(err);
+    }
   };
   return (
     <div className="p-6 m-auto mt-5 shadow rounded-md bg-white ">
